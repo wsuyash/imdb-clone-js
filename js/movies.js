6ab8d4bb0c17638b5c553585b4922382cb57972c
wsuyash <suyashcr07@gmail.com>
2022-01-30 12:34:53 +0530

    Add add-to-fav functionality and rating to Movies page

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,7 @@
 const API = 'http://www.omdbapi.com/?apikey=bd2e447b&i=';
 
+let favMovies = JSON.parse(localStorage.getItem("fav_movies")) || [];
+
 getMovie = async () => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -15,6 +17,29 @@ getMovie = async () => {
     document.getElementById('movie-info-name').innerHTML = response.Title;
     document.getElementById('movie-info-year').innerHTML = "(" + response.Year + ")";
     document.getElementById('movie-info-plot').innerHTML = response.Plot;
+    document.getElementById('movie-info-rating').innerHTML = response.imdbRating;
+    if (favMovies.find((movie) => { return movie.id === movieID })) {
+      document.getElementById('add-to-fav-btn').remove();
+    }
   }
+
 }
 window.addEventListener('load', getMovie);
+
+saveToFavs = (e) => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const id = urlParams.get('id');
+
+  let movie = {
+    id: id,
+    poster: document.getElementById('movie-info-poster').getAttribute('src'),
+    title: document.getElementById('movie-info-name').innerHTML,
+    year: document.getElementById('movie-info-year').innerHTML,
+  };
+
+  favMovies.push(movie);
+  localStorage.setItem("fav_movies", JSON.stringify(favMovies));
+  e.target.remove();
+}
+document.getElementById('add-to-fav-btn').addEventListener('click', saveToFavs);
